3f70f36efa1e4a13fa58298fa6d964ed64bc598a
felipemarin93 <felipemaringiraldo@gmail.com>
2022-07-21 13:27:52 -0500

    Se implementa la interfaz de MongoRepository para los métodos de player

--- domain/model/src/main/java/co/com/sofkau/model/player/gateways/PlayerRepository.java
+++ domain/model/src/main/java/co/com/sofkau/model/player/gateways/PlayerRepository.java
@@ -1,13 +1,16 @@
 package co.com.sofkau.model.player.gateways;
 
+import co.com.sofkau.model.carta.Carta;
 import co.com.sofkau.model.player.Player;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface PlayerRepository {
     Mono <Player> save(Player player);
-    Mono <Player> findById(String playerId);
+    Flux<Player> findAll();
+    Mono<Player> findById(String playerId);
     Mono <Player> update(String playerId, Player player);
-    Mono <Player> deleteById(String playerId);
+    Mono <Void> deleteById(String playerId);
 
 
 }
--- infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/sofkau/mongo/card/MongoRepositoryAdapterCard.java
+++ infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/sofkau/mongo/card/MongoRepositoryAdapterCard.java
@@ -13,12 +13,7 @@ implements CartaRepository
 {
 
     public MongoRepositoryAdapterCard(MongoDBRepositoryCard repository, ObjectMapper mapper) {
-        /**
-         *  Could be use mapper.mapBuilder if your domain model implement builder pattern
-         *  super(repository, mapper, d -> mapper.mapBuilder(d,ObjectModel.ObjectModelBuilder.class).build());
-         *  Or using mapper.map with the class of the object model
-         */
-        super(repository, mapper, d -> mapper.map(d, Carta.class/* change for domain model */));
+        super(repository, mapper, d -> mapper.map(d, Carta.class));
     }
 
     @Override
