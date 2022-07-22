9997fd2688f73f9a415979bfa1a0aecebb3bec85
felipemarin93 <felipemaringiraldo@gmail.com>
2022-07-22 12:12:22 -0500

    Se realiza el Caso de uso para asignar las cartas al jugador

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/sofkau/api/RouterRest.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/sofkau/api/RouterRest.java
@@ -3,6 +3,7 @@ package co.com.sofkau.api;
 import co.com.sofkau.api.board.HandlerBoard;
 import co.com.sofkau.api.card.HandlerCard;
 import co.com.sofkau.api.game.HandlerGame;
+import co.com.sofkau.api.player.HandlerPlayer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -15,7 +16,7 @@ import static org.springframework.web.reactive.function.server.RequestPredicates
 @Configuration
 public class RouterRest {
 @Bean
-public RouterFunction<ServerResponse> routerFunction(HandlerCard handlerCard, HandlerBoard handlerBoard, HandlerGame handlerGame) {
+public RouterFunction<ServerResponse> routerFunction(HandlerCard handlerCard, HandlerBoard handlerBoard, HandlerGame handlerGame, HandlerPlayer handlerPlayer) {
     return route(GET("/api/card/listcards"), handlerCard::listCards)
     .andRoute(POST("/api/card/createcard"), handlerCard::createCard)
             .andRoute(GET("/api/card/listcard/{id}"),handlerCard::findCardById)
@@ -24,7 +25,12 @@ public RouterFunction<ServerResponse> routerFunction(HandlerCard handlerCard, Ha
             .andRoute(GET("/api/board/listboard/{id}"), handlerBoard::findBoardById)
             .andRoute(POST("/api/board/createboard"), handlerBoard::createBoard)
             .andRoute(GET("/api/game/listgame"),handlerGame::listGame)
-            .andRoute(POST("/api/game/createGame"),handlerGame::createGame);
+            .andRoute(POST("/api/game/createGame"),handlerGame::createGame)
+            .andRoute(POST("/api/player/createplayer"), handlerPlayer::createPlayer)
+            .andRoute(GET("/api/player/listplayer/{id}"),handlerPlayer::findPlayerById)
+            .andRoute(PUT("/api/player/updateplayer/{id}"),handlerPlayer::updatePlayer)
+            .andRoute(DELETE("api/player/deleteplayer/{id}"),handlerPlayer::deletePlayerById)
+            .andRoute(POST("api/player/assigncards/{id}"),handlerPlayer::assignCardToPlayer);
 
     }
 }
