8053dad55271900589b4dc860c16111ad59ea96d
erickdiaz01 <erdiazb@unal.edu.co>
2022-07-21 23:52:11 -0500

    Logica de creaccion en cascada

--- domain/usecase/src/main/java/co/com/sofkau/usecase/game/creategame/createGameUseCase.java
+++ domain/usecase/src/main/java/co/com/sofkau/usecase/game/creategame/createGameUseCase.java
@@ -1,5 +1,7 @@
 package co.com.sofkau.usecase.game.creategame;
 
+import co.com.sofkau.model.board.Board;
+import co.com.sofkau.model.board.gateways.BoardRepository;
 import co.com.sofkau.model.game.Game;
 import co.com.sofkau.model.game.gateways.GameRepository;
 import lombok.RequiredArgsConstructor;
@@ -8,7 +10,9 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class createGameUseCase {
     private final GameRepository gameRepository;
+    private final BoardRepository boardRepository;
     public Mono<Game> SaveGame(Game game){
+        game.setBoard(boardRepository.save(game.getBoard()).toFuture().join());
         return gameRepository.save(game);
     }
 }
