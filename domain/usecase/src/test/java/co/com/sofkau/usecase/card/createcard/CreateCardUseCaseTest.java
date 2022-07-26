e5a7701284939d3874d3b44476b386e2182e062f
felipemarin93 <felipemaringiraldo@gmail.com>
2022-07-25 20:04:15 -0500

    Se realiza el perimer test unitario

---
+++ domain/usecase/src/test/java/co/com/sofkau/usecase/card/createcard/CreateCardUseCaseTest.java
@@ -0,0 +1,19 @@
+package co.com.sofkau.usecase.card.createcard;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateCardUseCaseTest {
+
+//    @BeforeEach
+//    void setUp() {
+//        MockitoAnnotations.openMocks(this)}
+
+
+    @Test
+    void createCard() {
+    }
+}
