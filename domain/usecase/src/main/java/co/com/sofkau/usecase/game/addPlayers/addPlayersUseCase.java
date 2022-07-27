82e98c34beebb950e6985041ae4f11be60036282
felipemarin93 <felipemaringiraldo@gmail.com>
2022-07-27 13:07:04 -0500

    Cambios

--- domain/usecase/src/main/java/co/com/sofkau/usecase/game/addPlayers/addPlayersUseCase.java
+++ domain/usecase/src/main/java/co/com/sofkau/usecase/game/addPlayers/addPlayersUseCase.java
@@ -16,20 +16,8 @@ import reactor.core.publisher.Mono;
 public class addPlayersUseCase {
     private final GameRepository gameRepository;
     private final FindGameByIdUseCase findGameByIdUseCase;
-<<<<<<< HEAD
-
-
-    /**
-     * función de tipo Mono que guarda un jugador en el juego recibiendo por parametro el
-     * @param id
-     * @param player
-     * se crea el juego de tipo Game a la cual se le añade el jugador recibido por parámetro
-     * @return el repositorio
-     */
-    public Mono<Game> savePlayer(String id , Player player){
-        var game = findGameByIdUseCase.findGameById(id).toFuture().join();
-=======
     private final DealCardsUseCase dealCardsUseCase;
+
 
     /**
      * Metodo que recibe por parametros el id del juego a modificar y el Player a adicionar
@@ -40,7 +28,7 @@ public class addPlayersUseCase {
      */
     public Mono<Game> savePlayer(String gameId , Player player){
         var game = findGameByIdUseCase.findGameById(gameId).toFuture().join();
->>>>>>> main
+
         game.getPlayers().add(player);
         if(game.getPlayers().size()>=2){
             dealCardsUseCase.dealCards(gameId,game);
